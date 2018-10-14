38b7eb888d00395bdb52ae4b4ca47317a955422b
Nolan Scobie <nolan.scobie@gmail.com>
2018-10-14 01:21:49 -0400

    ageag

--- AndroidStudioProjects/MedBot/app/src/main/java/health/medbot/medbot/MainActivity.java
+++ AndroidStudioProjects/MedBot/app/src/main/java/health/medbot/medbot/MainActivity.java
@@ -1,7 +1,9 @@
 package health.medbot.medbot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickSearch(View view) {
+        Intent intent = new Intent(getBaseContext(), ViewPatientActivity.class);
+        intent.putExtra("pid", 1);
+        startActivity(intent);
+    }
+
+    public void clickNewPatient(View view) {
+
     }
 }
--- AndroidStudioProjects/MedBot/app/src/main/java/health/medbot/medbot/ViewPatientActivity.java
+++ AndroidStudioProjects/MedBot/app/src/main/java/health/medbot/medbot/ViewPatientActivity.java
@@ -2,6 +2,7 @@ package health.medbot.medbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewPatientActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class ViewPatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_patient);
+
+        int pid = getIntent().getIntExtra("pid", -1);
+        Patient patient = Patient.getPatient(pid);
+        loadPatientData(patient);
+    }
+
+    private void loadPatientData(Patient patient) {
+        TextView name = findViewById(R.id.name);
+        name.setText(patient.getName());
     }
 }
